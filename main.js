18557dd5f719bf21249bcf0f48b93b82f09c4b7b
wedrtsdad <88042808+wedrtsdad@users.noreply.github.com>
2022-05-10 17:29:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+noseX= 0;
+noseY= 0;
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+
+	canvas= createCanvas(1200,366);
+	canvas.parent("canvas");
+
+	video= createCapture(VIDEO);
+	video.size(800,400);
+	video.parent("game_console");
+	instializeInSetup(mario);
+
+	poseNet= ml5.poseNet(video,modeloaded);
+	poseNet.on('pose',g_r);
+	
+}
+  
+function modeloaded(){
+	console.log("bla.....bla...."); 
+}
+  
+function g_r(results){
+	if(results.length > 0){
+	  console.log(results);
+	  noseX= results[0].pose.nose.x;
+	  noseY= results[0].pose.nose.y;
+	   }
+  }
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
